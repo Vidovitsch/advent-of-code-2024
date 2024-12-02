37e3f2ab538da3fda0ff2093ac93414daa213a6d
David de Bekker <david.de.bekker@gmail.com>
2024-12-02 12:12:43 +0100

    Solve day 2

---
+++ solutions/2/1.py
@@ -0,0 +1,40 @@
+import pathlib
+import re
+from typing import List, Tuple
+
+from advent_helper.decorators import process_puzzle_input
+from advent_helper.puzzle import Puzzle
+
+CURRENT = pathlib.Path(__file__).parent
+
+PuzzleInput = List[List[int]]
+
+
+def process_input(input: List[str]) -> PuzzleInput:
+    return [[int(number) for number in line.split()] for line in input]
+
+
+@process_puzzle_input(process_input)
+def solve(input: PuzzleInput) -> int:
+    return sum(1 for report in input if is_save(report))
+
+
+def is_save(report) -> bool:
+    differences = [report[i + 1] - report[i] for i in range(len(report) - 1)]
+
+    if min(differences) < 0 and max(differences) > 0:
+        return False
+
+    for difference in differences:
+        if abs(difference) < 1 or abs(difference) > 3:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    (
+        Puzzle("Day 2: Red-Nosed Reports (part 1)", CURRENT / "input.txt")
+        .add_test({"input_path": CURRENT / "test_1.txt", "expected_result": 2})
+        .solve(solve)
+    )
